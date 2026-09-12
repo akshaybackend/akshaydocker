3e17663730f6b5109c619b34fbdce85df4952c53
akshaybackend <Akshaytandon09@gmail.com>
2026-09-12 12:42:08 +0530

    fiest

---
+++ src/main/java/com/javabackend/example/SpringBootDockerExmpleApplication.java
@@ -0,0 +1,21 @@
+package com.javabackend.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerExmpleApplication {
+
+    @GetMapping("/hello")
+    public String message(){
+        return "Hello from docker ";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerExmpleApplication.class, args);
+	}
+
+}
